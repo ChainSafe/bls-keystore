468765003e99cd8bc4c2331b7795c7ded5315248
Andrijan Ostrun <andrijan.ostrun@gmail.com>
2019-11-11 22:35:03 +0100

    linter fixes

--- src/keystore.ts
+++ src/keystore.ts
@@ -5,136 +5,136 @@ import { randomBytes } from "crypto";
 import {PublicKey, PrivateKey} from "@chainsafe/bls";
 
 export interface IKeystoreModule{
-    function: string,
-    params: ScryptParams | PBKDF2Params | any,
-    message: bytes
+  function: string;
+  params: ScryptParams | PBKDF2Params | any;
+  message: bytes;
 }
 
 export class KeystoreModule implements IKeystoreModule {
-    public function: string = "";
-    public params: ScryptParams | PBKDF2Params | any = {};
-    public message: bytes = new Buffer("");
+  public function = "";
+  public params: ScryptParams | PBKDF2Params | any = {};
+  public message: bytes = new Buffer("");
 
-    public static fromJson(json: IKeystoreModule): KeystoreModule {
-        let keystoreModule = new KeystoreModule();
-        keystoreModule.function = json["function"];
-        keystoreModule.params = json["params"];
-        keystoreModule.message = json["message"];
+  public static fromJson(json: IKeystoreModule): KeystoreModule {
+    const keystoreModule = new KeystoreModule();
+    keystoreModule.function = json["function"];
+    keystoreModule.params = json["params"];
+    keystoreModule.message = json["message"];
 
-        return keystoreModule;
-    }
+    return keystoreModule;
+  }
 }
 
 export interface IKeystoreCrypto{
-    kdf: IKeystoreModule,
-    checksum: IKeystoreModule,
-    cipher: IKeystoreModule
+  kdf: IKeystoreModule;
+  checksum: IKeystoreModule;
+  cipher: IKeystoreModule;
 }
 
 export class KeystoreCrypto implements IKeystoreCrypto {
 
-    public kdf: IKeystoreModule = new KeystoreModule();
-    public checksum: IKeystoreModule = new KeystoreModule();
-    public cipher: IKeystoreModule = new KeystoreModule();
+  public kdf: IKeystoreModule = new KeystoreModule();
+  public checksum: IKeystoreModule = new KeystoreModule();
+  public cipher: IKeystoreModule = new KeystoreModule();
 
-    public static fromJson(json: IKeystoreCrypto): KeystoreCrypto {
-        let keystoreCrypto = new KeystoreCrypto();
+  public static fromJson(json: IKeystoreCrypto): KeystoreCrypto {
+    const keystoreCrypto = new KeystoreCrypto();
 
-        keystoreCrypto.kdf = KeystoreModule.fromJson(json["kdf"]);
-        keystoreCrypto.checksum = KeystoreModule.fromJson(json["checksum"]);
-        keystoreCrypto.cipher = KeystoreModule.fromJson(json["cipher"]);
+    keystoreCrypto.kdf = KeystoreModule.fromJson(json["kdf"]);
+    keystoreCrypto.checksum = KeystoreModule.fromJson(json["checksum"]);
+    keystoreCrypto.cipher = KeystoreModule.fromJson(json["cipher"]);
 
-        return keystoreCrypto;
-    }
+    return keystoreCrypto;
+  }
 }
 
 export interface IKeystore{
-    crypto: IKeystoreCrypto,
-    pubkey: string,
-    path: string,
-    uuid: string,
-    version: number;
+  crypto: IKeystoreCrypto;
+  pubkey: string;
+  path: string;
+  uuid: string;
+  version: number;
 
-    decrypt(password: string): Buffer;
+  decrypt(password: string): Buffer;
 }
 
 export class Keystore implements IKeystore {
-    public crypto: IKeystoreCrypto = new KeystoreCrypto();
-    public pubkey: string = "";
-    public path: string = "";
-    public uuid: string = uuid.v4();
-    public version: number = 0;
+  public crypto: IKeystoreCrypto = new KeystoreCrypto();
+  public pubkey = "";
+  public path = "";
+  public uuid: string = uuid.v4();
+  public version = 0;
 
-    private kdf(password: string, args: object): Buffer{
-        if(this.crypto.kdf.function === "scrypt"){
-            return scrypt(password, <ScryptParams> args);
-        }else{
-            return PBKDF2(password, <PBKDF2Params> args);
-        }
-        // else if(this.crypto.kdf.function === "pbkdf2"){
-        //     return PBKDF2(password, <PBKDF2Params> args);
-        // }
+  private kdf(password: string, args: object): Buffer{
+    if(this.crypto.kdf.function === "scrypt"){
+      return scrypt(password, args as ScryptParams);
+    }else{
+      return PBKDF2(password, args as PBKDF2Params);
     }
+    // else if(this.crypto.kdf.function === "pbkdf2"){
+    //     return PBKDF2(password, <PBKDF2Params> args);
+    // }
+  }
 
-    public static fromJson(json: IKeystore): Keystore {
-        let keystore = new Keystore();
-        keystore.crypto = KeystoreCrypto.fromJson(json["crypto"]);
-        keystore.path = json["path"];
-        keystore.pubkey = json["pubkey"];
-        keystore.uuid = json["uuid"];
-        keystore.version = json["version"];
+  public static fromJson(json: IKeystore): Keystore {
+    const keystore = new Keystore();
+    keystore.crypto = KeystoreCrypto.fromJson(json["crypto"]);
+    keystore.path = json["path"];
+    keystore.pubkey = json["pubkey"];
+    keystore.uuid = json["uuid"];
+    keystore.version = json["version"];
 
-        return keystore;
-    }
+    return keystore;
+  }
 
-    public static encrypt(secret: bytes, password: string, path: string = "", kdfSalt: bytes = randomBytes(32), aesIv: bytes = randomBytes(16)): IKeystore {
-        let keystore = new this();
+  public static encrypt(secret: bytes, password: string, path = "", kdfSalt: bytes = randomBytes(32), aesIv: bytes = randomBytes(16)): IKeystore {
+    const keystore = new this();
 
-        keystore.crypto.kdf.params.salt = kdfSalt;
-        const decryptionKey: bytes = keystore.kdf(password, keystore.crypto.kdf.params);
-        keystore.crypto.cipher.params.iv = aesIv;
-        const cipher = AES_128_CTR(decryptionKey.slice(0, 16), keystore.crypto.cipher.params.iv);
+    keystore.crypto.kdf.params.salt = kdfSalt;
+    const decryptionKey: bytes = keystore.kdf(password, keystore.crypto.kdf.params);
+    keystore.crypto.cipher.params.iv = aesIv;
+    const cipher = AES_128_CTR(decryptionKey.slice(0, 16), keystore.crypto.cipher.params.iv);
         
-        let encryptedSecret = cipher.update(secret);
-        encryptedSecret = Buffer.concat([encryptedSecret, cipher.final()]);
+    let encryptedSecret = cipher.update(secret);
+    encryptedSecret = Buffer.concat([encryptedSecret, cipher.final()]);
         
-        keystore.crypto.cipher.message = Buffer.from(encryptedSecret);
-        keystore.crypto.checksum.message = SHA256(Buffer.concat([decryptionKey.slice(16, 32), keystore.crypto.cipher.message]));
+    keystore.crypto.cipher.message = Buffer.from(encryptedSecret);
+    keystore.crypto.checksum.message = SHA256(Buffer.concat([decryptionKey.slice(16, 32), keystore.crypto.cipher.message]));
         
-        keystore.pubkey = PublicKey.fromPrivateKey(PrivateKey.fromBytes(secret)).toHexString().substring(2);
-        keystore.path = path;
+    keystore.pubkey = PublicKey.fromPrivateKey(PrivateKey.fromBytes(secret)).toHexString().substring(2);
+    keystore.path = path;
 
-        return keystore;
-    }
+    return keystore;
+  }
 
-    public decrypt(password: string): Buffer {
+  public decrypt(password: string): Buffer {
 
-        const decryptionKey: bytes = this.kdf(password, this.crypto.kdf.params);
+    const decryptionKey: bytes = this.kdf(password, this.crypto.kdf.params);
         
-        const cipher = AES_128_CTR(decryptionKey.slice(0, 16), this.crypto.cipher.params.iv);
-        let decryptedSecret = cipher.update(this.crypto.cipher.message);
-        decryptedSecret = Buffer.concat([decryptedSecret, cipher.final()]);
-        return decryptedSecret;
-    }
+    const cipher = AES_128_CTR(decryptionKey.slice(0, 16), this.crypto.cipher.params.iv);
+    let decryptedSecret = cipher.update(this.crypto.cipher.message);
+    decryptedSecret = Buffer.concat([decryptedSecret, cipher.final()]);
+    return decryptedSecret;
+  }
 
 }
 
 export class Pbkdf2Keystore extends Keystore {
-    constructor(){
-        super();
-        this.crypto.kdf.function = "pbkdf2";
-        this.crypto.kdf.params = DefaultPBKDF2Params;
-        this.crypto.checksum.function = "sha256";
-        this.crypto.cipher.function = "aes-128-ctr"
-    }
+  constructor(){
+    super();
+    this.crypto.kdf.function = "pbkdf2";
+    this.crypto.kdf.params = DefaultPBKDF2Params;
+    this.crypto.checksum.function = "sha256";
+    this.crypto.cipher.function = "aes-128-ctr"
+  }
 }
 
 export class ScryptKeystore extends Keystore {
-    constructor(){
-        super();
-        this.crypto.kdf.function = "scrypt";
-        this.crypto.kdf.params = DefaultScryptParams;
-        this.crypto.checksum.function = "sha256";
-        this.crypto.cipher.function = "aes-128-ctr"
-    }
+  constructor(){
+    super();
+    this.crypto.kdf.function = "scrypt";
+    this.crypto.kdf.params = DefaultScryptParams;
+    this.crypto.checksum.function = "sha256";
+    this.crypto.cipher.function = "aes-128-ctr"
+  }
 }
