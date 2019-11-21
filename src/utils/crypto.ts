b3688a48858178868d6c6e3d1c71bf6d675d29bf
Marin Petrunić <marin.petrunic@gmail.com>
2019-11-21 13:51:17 +0100

    fix native scrypt and pbkdf2 in electron

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,14 +1,12 @@
 import {bytes} from "..";
 import {default as SHA256Hash} from "bcrypto/lib/sha256";
-import {derive as secryptDerive} from "bcrypto/lib/scrypt";
-import {derive as pbkdf2Derive} from "bcrypto/lib/pbkdf2";
 import {Cipher} from "bcrypto/lib/cipher";
 import {IPBKDF2ModuleParams, IScryptModuleParams, KdfModuleParams} from "../crypto/module/params";
 import {Buffer} from "buffer";
 import {CryptoFunction} from "../crypto/module";
 
 export function scrypt(password: string, params: IScryptModuleParams): Buffer{
-  return secryptDerive(Buffer.from(password), params.salt, params.n, params.r, params.p, params.dklen);
+  return scryptDerive(Buffer.from(password), params.salt, params.n, params.r, params.p, params.dklen);
 }
 
 export function PBKDF2(password: string, params: IPBKDF2ModuleParams): Buffer{
@@ -39,10 +37,28 @@ export function kdf(password: string, func: CryptoFunction, args: KdfModuleParam
 }
 
 export function randomBytes(length: number): Buffer {
-  //electron replaces openssl with boressl which causes incompatibilities with randomness
+  //electron replaces openssl with boressl which causes incompatibilities
   if(process && process.env.ELECTRON) {
     return require("bcrypto/lib/node/random").randomBytes(length);
   } else {
     return require("bcrypto/lib/random").randomBytes(length);
+  }
+}
+
+function pbkdf2Derive(hash: Function, pass: Buffer | string, salt: Buffer | string, iter: number, len: number) {
+  //electron replaces openssl with boressl which causes incompatibilities
+  if(process && process.env.ELECTRON) {
+    return require("bcrypto/lib/node/pbkdf2").derive(hash, pass, salt, iter, len);
+  } else {
+    return require("bcrypto/lib/pbkdf2").derive(hash, pass, salt, iter, len);
+  }
+}
+
+function scryptDerive(passwd: Buffer | string, salt: Buffer | string, N: number, r: number, p: number, len: number) {
+  //electron replaces openssl with boressl which causes incompatibilities
+  if(process && process.env.ELECTRON) {
+    return require("bcrypto/lib/node/scrypt").derive(passwd, salt, N, r, p, len);
+  } else {
+    return require("bcrypto/lib/scrypt").derive(passwd, salt, N, r, p, len);
   }
 }
