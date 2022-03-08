954a70951f74daf2e46ff34772ee0bb1bd8a4d03
Dadepo Aderemi <dadepo@gmail.com>
2022-03-08 09:42:13 +0100

    using hex literals in filtering control code

--- src/password.ts
+++ src/password.ts
@@ -2,9 +2,11 @@ import { utf8ToBytes } from "ethereum-cryptography/utils";
 
 /**
  * Normalizes password to NFKD representation and strips the C0, C1, and Delete control codes.
- * C0 are the control codes between 0x00 - 0x1F (inclusive) | 0 - 31
- * C1 codes lie between 0x80 and 0x9F (inclusive) | 128 - 159
- * Delete, commonly known as “backspace”, is the UTF-8 character  | 127
+ * C0 are the control codes between 0x00 - 0x1F (inclusive)
+ * C1 codes lie between 0x80 and 0x9F (inclusive)
+ * Delete, commonly known as “backspace”, is the UTF-8 character
+ *
+ * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-2335.md#password-requirements
  */
 export function normalizePassword(password: string | Uint8Array): Uint8Array {
   if (typeof password === "string") {
@@ -20,5 +22,5 @@ export function normalizePassword(password: string | Uint8Array): Uint8Array {
 
 
 function controlCodeFilter(charCode: number): boolean {
-  return (charCode > 31) && !(charCode >= 127 && charCode <= 159)
+  return (charCode > 0x1F) && !(charCode >= 0x7f && charCode <= 0x9F)
 }
