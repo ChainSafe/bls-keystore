879d8c6e2814929758a66703630f7c60d46c8af5
Dadepo Aderemi <dadepo@gmail.com>
2022-03-08 09:44:23 +0100

    completely removed a line comment

--- src/password.ts
+++ src/password.ts
@@ -4,7 +4,6 @@ import { utf8ToBytes } from "ethereum-cryptography/utils";
  * Normalizes password to NFKD representation and strips the C0, C1, and Delete control codes.
  * C0 are the control codes between 0x00 - 0x1F (inclusive)
  * C1 codes lie between 0x80 and 0x9F (inclusive)
- * Delete, commonly known as “backspace”, is the UTF-8 character
  *
  * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-2335.md#password-requirements
  */
