789b8ba3c571ab1d98a9eb58df71cfcbc3b0d765
Cayman <caymannava@gmail.com>
2024-07-15 13:49:02 -0400

    chore: add comments

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,8 @@
+/** Is in nodejs environment */
 export const isNode =
   typeof process !== "undefined" &&
   process.versions != null &&
   process.versions.node != null;
 
+/** Is in environment with web crypto */
 export const hasWebCrypto = globalThis?.crypto?.subtle != null;
